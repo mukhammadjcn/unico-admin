db31155cd02da4b200072a7613eb5ef1cd20b233
mukhammadjcn <mukhammadjcn@gmail.com>
2023-09-12 17:58:51 +0500

    changed baseurl

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { createBrowserHistory } from 'history';
 const history = createBrowserHistory({ window });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HistoryRouter history={history} basename="/showroom">
+  <HistoryRouter history={history} basename="/">
     <Routes>
       <Route index element={<App />} />
     </Routes>
